d8c073429ed71f0425cd310e5cc4f943c050240a
Sebastian Kann <sebastiankann@gmail.com>
2020-09-28 22:05:03 +0000

    added urls_new template and configured a POST response for /urls in the server file

--- express_server.js
+++ express_server.js
@@ -2,32 +2,42 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+const generateRandomString = () => Math.random().toString(36).substring(2, 8);
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`A = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`A = ${a}`);
-});
-
 app.get("/urls", (req, res) => {
-  res.json(urlDatabase);
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[this.shortURL],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
