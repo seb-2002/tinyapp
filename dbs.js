da29ce2ba9907468cbb6c9410dd0016c23f5203a
Sebastian Kann <sebastiankann@gmail.com>
2020-10-01 20:34:36 +0000

    moved databases into a  dbs.js file

---
+++ dbs.js
@@ -0,0 +1,25 @@
+const { hash } = require("./helpers");
+
+// "DATABASES"
+const urlDatabase = {
+  b2xVn2: { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" },
+};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: hash("purple-monkey-dinosaur"),
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: hash("dishwasher-funk"),
+  },
+};
+
+module.exports = {
+  urlDatabase,
+  users,
+};
